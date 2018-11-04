111106355613295b1a5ac637a08f5b20cfdffc53
SupahAmbition <tylerbeverley@gmail.com>
2018-11-04 14:38:01 -0600

    bitpacking works correctly

--- assembler.ml
+++ assembler.ml
@@ -6,6 +6,18 @@
  * 4. Print the resulting int to the command line, or to a specified output file. 
  *)
 
+
+(* just for debugging *)
+let printIntElements lst = 
+    
+    let helper str intopt = 
+        match intopt with 
+        | None -> "none " ^ str
+        | Some x -> (Pervasives.string_of_int x) ^ " " ^ str 
+    
+    in List.fold_left helper "" lst
+;;
+
 (* determineOppcode 
  * string -> int 
  *
@@ -38,7 +50,7 @@ let determineOppcode str =
 let instructParser instructs = 
     
     let helper line = 
-        Str.bounded_split_delim (Str.regexp "[ \n\r\t]+") line 6;
+        Str.bounded_split_delim (Str.regexp "[ \n\r\x0c\t]+") line 6;
     in 
     List.map helper instructs;  
 ;;
@@ -49,7 +61,7 @@ let instructParser instructs =
  *
  * This function takes a list of assembly instructions
  * as strings, and will convert them to their respective int values*)
-let toInt instruction = 
+let toInt lineNumber instruction = 
 
     (* This helper will take an the element index and
      * the element from the instruction, and return either some 
@@ -59,10 +71,13 @@ let toInt instruction =
     let helper elemNum elem =
         match elemNum with 
         | 0 -> None (* Is a tag *)
+       
         | 1 -> Some(determineOppcode elem) (* is a oppcode *)
+       
+        (* translate fields 1 and 2 to int *) 
         | 2 
-        | 3
-        | 4 -> Some( int_of_string elem) (* translate fields 1,2,3 to int *)
+        | 3 
+        | 4 -> Some( int_of_string elem) 
         | _ -> None 
     
     in 
@@ -72,64 +87,45 @@ let toInt instruction =
 ;;
 
 
-(*
-
-let packRtype instruct = 
-    let oppcode = List.nth  instruct 1 in
-    let destR   = List.nth  instruct 2 in 
-    let reg1    = List.nth  instruct 3 in 
-    let reg2    = List.nth  instruct 4 in 
-   
-    let result = 0x0 
-    lor (oppcode lsl 22) 
-    lor (destR lsl 19) 
-    lor (reg1 lsl 16) 
-    lor (reg2) 
-    in result; 
-;;
-*)
-
-
 (* tag oppcode destR reg1 reg2 comments *)
 (* packInt 
  * int option list -> int 
  *
  * this function takes in a list a of instruct elements 
- * and will returned the correctly packed int. *)
+ * and will returned the correctly packed int. 
+ * This function uses mutation on an int refrence 
+ * to calculate the packed int*)
 let packInt instruct =
 
     let instructIntOpts = List.filter (fun x -> x != None) instruct in 
-    let instructInts = List.map (fun x -> 
+    let instructInts = List.rev_map (fun x -> 
         match x with 
         | None -> failwith "there should be no none value in the list" 
         | Some y -> y ) instructIntOpts 
     in 
-    let result = ref 0 in  
+    
+    let result = ref 0x0 in  
     let helper elemNum elem  = 
         match elemNum with 
-        | 0 -> result := !result lor ( elem lsl 22) 
+        | 0 -> result := !result lor ( elem lsl 22);
         | 1 -> result := !result lor ( elem lsl 19) 
         | 2 -> result := !result lor ( elem lsl 16)
-        | 3 -> result := !result lor elem 
+        | 3 -> result := !result lor ( elem land 0x0000ffff) 
         | _ -> () 
     in List.iteri helper instructInts;
-    !result;
+    
+    if List.hd instructInts = 8
+        (* if the oppcode is fill then 
+            * just return the second element*)
+        then List.hd (List.tl instructInts) 
+    else 
+        !result;
 ;;
 
 
 
 
-(* just for debugging *)
-let printIntElements lst = 
-    
-    let helper str intopt = 
-        match intopt with 
-        | None -> "none " ^ str
-        | Some x -> (Pervasives.string_of_int x) ^ " " ^ str 
-    
-    in List.fold_left helper "" lst
-;;
-
+(* Getopt stuff *)
 
 let input  = ref "";;
 let output = ref "";;
@@ -156,11 +152,11 @@ let _ =
    let instruct_elements = instructParser inputList in 
    List.iter( fun x -> Printf.printf "%s \n" x ) inputList;
 
-   let instruct_ints = List.map (fun x -> toInt x) instruct_elements in 
+   let instruct_ints = List.mapi (fun x -> toInt x) instruct_elements in 
    List.iter( fun x -> 
        let str =  printIntElements  x in 
        Printf.printf "%s\n" str ) instruct_ints;
-   
+  
    List.iter (fun x ->
        let packed = packInt x in 
        Printf.printf "%d\n" packed ) instruct_ints; 
--- testcases/test1.asm
+++ testcases/test1.asm
@@ -2,8 +2,8 @@
 tag2	nand	1		0		1
 		lw		1		0		100
 		sw		1		0		200
-branch 	beq		0		0		100
-hello 	jalr	1		1
+branch	beq     0       0       100
+hello 	jalr    1       1
 		noop
 		halt
 		.fill 	3
